6efbeec241331862ec48721630f448133b10017d
Tiểu Long Nữ <48639620+hatan1410@users.noreply.github.com>
2020-12-31 00:42:17 +0700

    them mainController

--- src/controller/mainController.java
+++ src/controller/mainController.java
@@ -1,8 +1,10 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 
+import com.sun.tools.javac.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,6 +22,12 @@ public class mainController {
 	}
 
     @FXML
+    public void showVi(ActionEvent event) {
+        Pane view = getPage("vi");
+        mainpane.setCenter(view);
+    }
+
+    @FXML
     public void showThuChi(ActionEvent event) throws IOException, SQLException {
     	
     	FXMLLoader loader = new FXMLLoader(ThuChiController.class.getResource("/view/main/ThuChiView2.fxml"));
@@ -29,5 +37,29 @@ public class mainController {
 		mainpane.setCenter(thuchiPane);
 
     }
-	
+
+    @FXML
+    void showDanhmuc(ActionEvent event) throws IOException, SQLException {
+
+    }
+
+    @FXML
+    void showThongKe(ActionEvent event) {
+
+    }
+
+    public Pane getPage(String fileName){
+        Pane view = new Pane();
+        try {
+            URL fileUrl = Main.class.getResource("/view/main/"+ fileName + ".fxml");
+            if(fileName == null){
+                throw new java.io.FileNotFoundException("FXML file can't be found");
+            }
+            view = FXMLLoader.load(fileUrl);
+        } catch (Exception e) {
+            System.out.println("No page " +fileName+ " please check mainController");
+            e.printStackTrace();
+        }
+        return view;
+    }
 }
